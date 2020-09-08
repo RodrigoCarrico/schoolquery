68c61026ae705109d2eb3f1c6ba504542b4353bb
Carrico <rodrigo.carrico@totvs.com.br>
2020-09-08 21:53:54 +0100

    inclusao de swagger

---
+++ src/main/java/br/com/onboard/schoolquery/SchoolQueryApplication.java
@@ -0,0 +1,13 @@
+package br.com.onboard.schoolquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolQueryApplication.class, args);
+    }
+
+}
